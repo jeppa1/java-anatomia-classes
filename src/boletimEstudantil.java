4f33e6efe94e2e8b7a18bccd651019631a406b19
jeppa1 <jadson.jeppa@gmail.com>
2024-05-14 00:04:32 -0300

    boletim 001

---
+++ src/boletimEstudantil.java
@@ -0,0 +1,22 @@
+public class boletimEstudantil {
+    public static void main(String[] args) {
+        // EXEMPLO SEM IDENTAÇÃO:
+        // int mediaFinal = 6;
+        // if(mediaFinal<6)
+        // System.out.printIn("REPROVADO");
+        // else if (mediaFinal==6)
+        // System.out.prinIn("PROVA FINAL");
+        // else
+        // System.out.printIn("APROVADO");
+
+        //EXEMPLO COM IDENTAÇÃO:
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA FINAL");
+        else
+            System.out.println("APROVADO");
+
+    }
+}
